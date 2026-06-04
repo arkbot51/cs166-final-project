e5ce088ea096c8567dece616674c409419c3c1ce
hwong070 <hwong070@ucr.edu>
2026-06-03 21:58:02 -0700

    Added more functions to buyers and sellers

--- final_project/src/app.py
+++ final_project/src/app.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from psycopg2 import extras, connect
 
 # --- DATABASE CONNECTION HELPER ---
@@ -31,10 +32,34 @@ def run_query(query, params=None, is_select=True):
 
 class AuctionCLI:
     def __init__(self):
-        self.current_user = "buyer_jack"
-        self.current_role = "Buyer"
+        self.current_user = None
+        self.current_role = None
+    
+    def login(self):
+        print("\n--- LOGIN ---")
+        username = input("Username: ").strip()
+        password = input("Password: ").strip()
+        login_sql = """
+            SELECT login, role
+            FROM users
+            WHERE login = %s AND password = %s
+        """
+
+        result = run_query(login_sql, (username, password), fetch = True)
+
+        if result:
+            self.current_user = result[0]["login"]
+            self.current_role = result[0]["role"]
+            print(f"\nLogin successful as {self.current_role}")
+            return True
+            
+        print("Invalid username or password. Please try again.")
+        return False
 
     def main_menu(self):
+        if not self.login():
+            return
+        
         while True:
             print("\n" + "="*45)
             print("AUCTION MARKETPLACE")
@@ -95,16 +120,22 @@ class AuctionCLI:
             print("-"*40)
             print("1. Browse Active Public Auctions")
             print("2. Place a Competitive Bid")
-            print("3. Return to Main Menu")
+            print("3. View Won Auction")
+            print("4. Make Payment")
+            print("5. Return to Main Menu")
             print("-"*40)
             
-            choice = input("Select choice (1-3): ").strip()
+            choice = input("Select choice (1-4): ").strip()
             
             if choice == "1":
                 self.view_active_auctions()
             elif choice == "2":
                 self.place_bid()
             elif choice == "3":
+                self.view_won_auctions()
+            elif choice == "4":
+                self.make_payment()
+            elif choice == "5":
                 break
 
     def view_active_auctions(self):
@@ -147,6 +178,56 @@ class AuctionCLI:
             run_query(update_sql, (amount, auc_id), is_select=False)
             print(f"\nsuccess: Your bid was processed! Saved Bid ID: {random_bid_id}")
 
+    def view_won_auctions(self):
+        won_auction_sql = """
+            SELECT auction_id, current_highest_bid, auction_status
+            FROM auction
+            WHERE winner_login = %s AND auction_status = "Closed"
+        """
+
+        result = run_query(won_auction_sql, (self.current_user,))
+        
+        print("\n--- YOUR WON AUCTIONS ---")
+        if not result:
+            print("You have not won any auctions yet.")
+            return
+        
+        for r in result:
+            print(r)
+
+    def make_payment(self):
+        auction_id = input("Enter Auction ID to pay for: ")
+        payment_sql = """
+            SELECT auction_id, winner_login, auction_status, current_highest_bid
+            FROM auction
+            WHERE auction_id = %s
+        """
+        
+        result = run_query(payment_sql, (auction_id,))
+
+        if not result:
+            print("Auction not found!")
+            return
+        
+        auction = result[0]
+
+        if auction["winner_login"] != self.current_user:
+            print("You are not the winner of this auction.")
+            return
+        
+        if auction["auction_status"] != "Closed":
+            print("Auction is not closed yet")
+            return
+        
+        payment_id = random.randint(1000,9999)
+
+        run_query("""
+            INSERT INTO payment (payment_id, auction_id, buyer_login, amount, payment_status)
+            VALUES (%s, %s, %s, %s, "Completed")
+        """, (payment_id, auction_id, self.current_user, auction["current_highest_bid"]), fetch = False
+        )
+
+        print("Payment is successful!")
     # -------------------------------------------------------------
     # 2. SELLER HUB
     # -------------------------------------------------------------
@@ -156,7 +237,10 @@ class AuctionCLI:
             print("SELLER HUB DASHBOARD")
             print("-"*40)
             print("1. Launch a New Public Auction Listing")
-            print("2. Return to Main Menu")
+            print("2. View my Auction")
+            print("3. Close Auction")
+            print("4. Create Shipment")
+            print("5. Return to Main Menu")
             print("-"*40)
             
             choice = input("Select choice (1-2): ").strip()
@@ -164,6 +248,12 @@ class AuctionCLI:
             if choice == "1":
                 self.create_listing()
             elif choice == "2":
+                self.view_my_auctions()
+            elif choice == "3":
+                self.close_auction()
+            elif choice == "4": 
+                self.create_shipment()
+            elif choice == "5":
                 break
 
     def create_listing(self):
@@ -192,8 +282,52 @@ class AuctionCLI:
                 VALUES (%s, %s, %s, 'Seller', %s, 'Active')
             """
             run_query(auc_sql, (auc_id, item_id, self.current_user, price), is_select=False)
-            print(f"\nsuccess: Item inventory loaded! Auction is live at ID: {auc_id}")
+            print(f"\nSuccess: Item inventory loaded! Auction is live at ID: {auc_id}")
 
+    def view_my_auctions(self):
+        result = run_query("""
+            SELECT *
+            FROM auction
+            WHERE seller_login = %s
+        """, (self.current_user,))
+
+        for r in result:
+            print(r)
+    
+    def close_auction(self):
+        auction_id = input("Auction ID for closing: ")
+
+        winner = run_query("""
+            SELECT buyer_login
+            FROM bid
+            WHERE auction_id = %s
+            ORDER BY bid_amount DESC LIMIT 1
+        """, (auction_id,))
+
+        if winner:
+            winner_login = winner[0]["buyer_login"]
+        else:
+            winner_login = None
+
+        run_query("""
+            UPDATE auction
+            SET auction_status = "Closed", winner_login = %s
+            WHERE auction_id = %s
+        """, (winner_login, auction_id), fetch = False)
+
+        print("Auction closed!")
+    
+    def create_shipment(self):
+        shipment_id = random.randint(10000,99999)
+        auction_id = input("Auction ID: ")
+        address = input("Shipping Address: ")
+
+        run_query("""
+            INSERT INTO shipment(shipment_id, auction_id, address, shipment_status)
+            VALUES(%s, %s, %s, "Pending")
+        """, (shipment_id, auction_id, address), fetch = False)
+
+        print("Shipment Created!")
     # -------------------------------------------------------------
     # 3. ADMIN ANALYTICS CONTROL PANEL
     # -------------------------------------------------------------
