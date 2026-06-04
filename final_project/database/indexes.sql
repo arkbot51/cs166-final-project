fd6bbb846dd3d344ca4191c34da1ccc54d6d5881
hwong070 <hwong070@ucr.edu>
2026-06-03 18:10:18 -0700

    Added to indexes.sql

--- final_project/database/indexes.sql
+++ final_project/database/indexes.sql
@@ -1,3 +1,9 @@
+DROP INDEX IF EXISTS idx_auction_item;
+DROP INDEX IF EXISTS idx_item_seller;
+DROP INDEX IF EXISTS idx_auction_status;
+DROP INDEX IF EXISTS idx_bid_buyer;
+DROP INDEX IF EXISTS idx_bid_auction;
+
 -- for joining items table and active auctions
 create index idx_auction_item on auction(item_id);
 create index idx_item_seller on item(seller_login);
@@ -7,3 +13,6 @@ create index idx_auction_status on auction(auction_status);
 
 -- for looking up bid history
 create index idx_bid_buyer on bid(buyer_login);
+
+-- for faster retrieval of bids 
+CREATE INDEX idx_bid_auction ON bid(auction_id);
